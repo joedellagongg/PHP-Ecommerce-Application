1c8d87d20661ff5c089bc6b81d0a4fb538bcf29c
JoedelLagong <joedel.lagong23@gmail.com>
2024-05-31 17:33:19 +0800

    added contact html

--- hi.php
+++ hi.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document HI AOAOAOAOA</title>
+    <title>Document HI Update Title</title>
 </head>
 <body>
 
